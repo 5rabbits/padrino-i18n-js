3fc80b7fbc052eb15eb9927b392b80f5370b8e0d
Abraham (ElDude) Barrera <abarrerac@gmail.com>
2015-08-04 11:22:47 -0300

    Update module

--- lib/padrino_i18n-js.rb
+++ lib/padrino_i18n-js.rb
@@ -1,19 +1,23 @@
-module FiveRabbits
-  module I18n
-    ## Rails module wrapper
-    module Rails
-      def self.version
-        '4.2.3'
-      end
-
-      def self.configuration
-      end
-
-      def self.root
-        Pathname.new(PADRINO_ROOT)
-      end
+module I18n
+  ## Rails module wrapper
+  module Rails
+    def version
+      '4.2.3'
     end
+
+    def env
+      Padrino.env.to_s
+    end
+
+    def root
+      Padrino.root
+    end
+
+    module_function :version
+    module_function :env
+    module_function :root
   end
 end
+
 
 require 'i18n-js'
